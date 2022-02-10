ed7170b97ab4d3cd3fb5a30819d43a8c1b61d8df
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 15:46:35 +0200

    fixed artist class

--- music/artist.py
+++ music/artist.py
@@ -1,11 +1,15 @@
+from logs.logging import info_log, debug_log, warning_log, error_log
 class Artist:
     def __init__(self, id, name):
         self.id = id
         self.name = name
-        self.albums = []
+        self.albums = {}
 
     def __repr__(self):
-        return f'id: {self.id} name: {self.name} albums: {self.albums}'
+        return f'name: {self.name} albums: {self.albums} '
 
+    #logs here?
     def add_album(self, album):
-        self.albums.append(album)
+        #info_log(__name__, self.add_album.__name__, "Entered into the function")
+        self.albums[album.get('id')] = album.get('name')
+        #info_log(__name__, self.add_album.__name__, "The function ended successfully")
