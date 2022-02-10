f89c3be97b551688c0dd07237dab24c712c5731a
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 16:15:33 +0200

    fixed Free_user class

--- user/free_user.py
+++ user/free_user.py
@@ -1,10 +1,9 @@
 from user.user import User
 
-
-
-
 class Free_user(User):
+    pass
 
-    @deco
-    def create_playlist(self, name, songs):
-        super().create_playlist(name, songs)
+#
+#    @deco
+ #   def create_playlist(self, name, songs):
+  #      super().create_playlist(name, songs)
