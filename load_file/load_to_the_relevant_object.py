1c65d2c9de92c73c48e4e9b47d5aa8a5642c4b7b
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 15:45:22 +0200

    fixed

--- load_file/load_to_the_relevant_object.py
+++ load_file/load_to_the_relevant_object.py
@@ -1,9 +1,10 @@
 from music.album import Album
 from music.artist import Artist
 from music.song import Song
+from logs.logging import info_log, debug_log, warning_log, error_log
 
-
-def albums_parser(data_files):
+def albums_load(data_files):
+    info_log(__name__, albums_load.__name__, "Entered into the function")
     all_albums = {}
     for data_file in data_files:
         songs = data_file.get('track')
@@ -15,10 +16,12 @@ def albums_parser(data_files):
                 all_albums[new_album.id] = (new_album)
             else:
                 Album.add_song(songs.get('album'), songs.get('id'))
+    info_log(__name__, albums_load.__name__, "The function ended successfully")
     return all_albums
 
 
-def artists_parser(data_files):
+def artists_load(data_files):
+    info_log(__name__, artists_load.__name__, "Entered into the function")
     all_artists = {}
     for data_file in data_files:
         songs = data_file.get('track')
@@ -30,12 +33,15 @@ def artists_parser(data_files):
                 all_artists[artist.get('id')] = new_artist
             else:
                 Artist.add_album(all_artists.get(artist.get('id')), songs.get('album'))
+    info_log(__name__, artists_load.__name__, "The function ended successfully")
     return all_artists
 
 
-def songs_parser(data_files):
+def songs_load(data_files):
+    info_log(__name__, songs_load.__name__, "Entered into the function")
     all_songs = {}
     for data_file in data_files:
         songs = data_file.get('track')
         all_songs[songs.get('id')] = Song(songs.get('id'), songs.get('name'),songs.get('popularity'))
+    info_log(__name__, songs_load.__name__, "The function ended successfully")
     return all_songs
