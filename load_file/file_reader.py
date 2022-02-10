e7fadf13aaf42d0fc5595a5e7ed11fd0f9fc6138
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 15:44:22 +0200

    fixed

--- load_file/file_reader.py
+++ load_file/file_reader.py
@@ -1,10 +1,17 @@
 import os
 import json
+from logs.logging import info_log, debug_log, warning_log, error_log
 
 def read_files(directory_path):
+    info_log(__name__, read_files.__name__, "Entered into the function")
     files = os.listdir(directory_path)
     data = []
     for file in files:
-        with open(directory_path + "\\" + file) as json_file:
-            data.append(json.load(json_file))
+        try:
+            with open(directory_path + "\\" + file) as json_file:
+                data.append(json.load(json_file))
+        except:
+            error_log(__name__, read_files.__name__, "No such file or directory")
+            break
+    info_log(__name__, read_files.__name__, "The function ended successfully")
     return data
