035846d99d6282c0b9d54d970d55462bdd08a598
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 15:52:38 +0200

    Added Main cladd

--- main/main.py
+++ main/main.py
@@ -1,8 +1,12 @@
 from load_file.file_reader import read_files
-from load_file.file_parser import albums_parser, songs_parser, artists_parser
+from load_file.load_to_the_relevant_object import albums_load, songs_load, artists_load
+from user.user import User
+from search.search import get_all_albums, get_all_artists, get_songs_of_album, get_albums_names_of_artist, get_top_10
 
 class Main:
-    list = read_files("C:\songs")
-    print(songs_parser(list))
-    print(albums_parser(list))
-    print(artists_parser(list))
+   # print(get_all_artists())
+    user1 = User("325", "linoy")
+    user1.create_playlist("love", "123, 345")
+    user1.create_playlist("love", "123, 345")
+
+    print(user1)
