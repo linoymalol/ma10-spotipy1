becc2f67651537897417efc9add583afa529cab2
Linoy Malol <linoymalol1@gmail.com>
2022-02-10 16:16:00 +0200

    fixed main

--- main/main.py
+++ main/main.py
@@ -1,12 +1,14 @@
 from load_file.file_reader import read_files
 from load_file.load_to_the_relevant_object import albums_load, songs_load, artists_load
 from user.user import User
+from user.free_user import Free_user
+from user.premium_user import Premium_user
 from search.search import get_all_albums, get_all_artists, get_songs_of_album, get_albums_names_of_artist, get_top_10
-
+import const
 class Main:
-   # print(get_all_artists())
-    user1 = User("325", "linoy")
-    user1.create_playlist("love", "123, 345")
-    user1.create_playlist("love", "123, 345")
 
+    user1 = Free_user("325", "linoy")
+    user1.create_playlist("love", ", 03DcpryHcONqKi2uKXK5Ow, 086myS9r57YsLbJpU0TgK9")
+    user1.create_playlist("love", "086myS9r57YsLbJpU0TgK9")
+    user1.create_playlist("shower", ", 03DcpryHcONqKi2uKXK5Ow, 086myS9r57YsLbJpU0TgK9")
     print(user1)
